f9cacb876608687e742c3517d13458a8c9f30867
Marc Cavazos <marcavazos19@icloud.com>
2022-02-26 17:33:37 -0600

    commit tree 3619ccc1fa3129cbd8a2e9ee999f6c9eebba37ea

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 20, 'M', 'dark brown', 'calm', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 6, 'M', 'Black', 'fiesty', 0);
+INSERT INTO bears VALUES (3, 'Tabitha', 4, 'F', 'White', 'calm', 0);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 5, 'M', 'Brown', 'intense', 1);
+INSERT INTO bears VALUES (5, 'Melissa', 8, 'F', 'Brown', 'nice', 0);
+INSERT INTO bears VALUES (6,'Grinch', 4, 'M', 'White', 'slimy', 1);
+INSERT INTO bears VALUES (7, 'Wendy', 12, 'F', 'Brown', 'goofy', 1); 
+INSERT INTO bears VALUES (8, NULL, 9, 'F', 'Black', 'aggressive', 1);
